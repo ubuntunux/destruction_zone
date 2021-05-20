c95537ba875c43894087375897e06fcea4ee5eb3
ubuntu <ubuntunux@gmail.com>
2021-05-20 21:49:56 +0900

    update

--- src/game_module/player_actor.rs
+++ src/game_module/player_actor.rs
@@ -80,7 +80,7 @@ impl PlayerActor {
         main_camera._transform_object.rotation_yaw(self._controller.get_velocity_yaw() * delta_time);
         main_camera._transform_object.update_transform_object();
 
-        let camera_pos = transform.get_position() + main_camera._transform_object.get_front() * CAMERA_OFFSET_Z + main_camera._transform_object.get_up() * CAMERA_OFFSET_Y;
+        let camera_pos = self._controller.get_position() + main_camera._transform_object.get_front() * CAMERA_OFFSET_Z + main_camera._transform_object.get_up() * CAMERA_OFFSET_Y;
         main_camera._transform_object.set_position(&camera_pos);
 
         let roll = self._controller.get_roll();
