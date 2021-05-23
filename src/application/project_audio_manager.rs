e3a8043ecceccaf6ca5bce81afd4aed2665d8e90
ubuntunux <ubuntunux@gmail.com>
2021-05-23 17:01:28 +0900

    update rodio multithread test

--- src/application/project_audio_manager.rs
+++ src/application/project_audio_manager.rs
@@ -1,54 +1,17 @@
+use std::io::{ Cursor, BufReader };
+
+use rodio::{self, source::Source, StreamError};
+use cpal;
+use cpal::traits::HostTrait;
+
 use rust_engine_3d::utilities::system::{ self, newRcRefCell, RcRefCell };
 
 use crate::application::project_application::ProjectApplication;
 use crate::resource::project_resource::ProjectResources;
 
-
-//
-extern crate sdl2;
-
-use sdl2::audio::{AudioCVT, AudioCallback, AudioSpecDesired, AudioSpecWAV};
-use std::path::{Path, PathBuf};
-use self::sdl2::audio::AudioDevice;
-
-// NOTE: You probably want to investigate the
-// mixer feature for real use cases.
-pub struct Sound {
-    data: Vec<u8>,
-    volume: f32,
-    pos: usize,
-}
-
-impl AudioCallback for Sound {
-    type Channel = u8;
-
-    fn callback(&mut self, out: &mut [u8]) {
-        for dst in out.iter_mut() {
-            // With channel type u8 the "silence" value is 128 (middle of the 0-2^8 range) so we need
-            // to both fill in the silence and scale the wav data accordingly. Filling the silence
-            // once the wav is finished is trivial, applying the volume is more tricky. We need to:
-            // * Change the range of the values from [0, 255] to [-128, 127] so we can multiply
-            // * Apply the volume by multiplying, this gives us range [-128*volume, 127*volume]
-            // * Move the resulting range to a range centered around the value 128, the final range
-            //   is [128 - 128*volume, 128 + 127*volume] – scaled and correctly positioned
-            //
-            // Using value 0 instead of 128 would result in clicking. Scaling by simply multiplying
-            // would not give correct results.
-            let pre_scale = *self.data.get(self.pos).unwrap_or(&128);
-            let scaled_signed_float = (pre_scale as f32 - 128.0) * self.volume;
-            let scaled = (scaled_signed_float + 128.0) as u8;
-            *dst = scaled;
-            self.pos += 1;
-        }
-    }
-}
-
-
-//
-
 pub struct AudioDataCreateInfo {
     pub _audio_name: String,
-    pub _audio_source: bool,
+    pub _audio_source: rodio::Decoder<BufReader<Cursor<Vec<u8>>>>,
 }
 
 #[derive(Clone)]
@@ -61,7 +24,8 @@ pub struct ProjectAudioManager {
     pub _project_resources: *const ProjectResources,
     pub _audios: Vec<RcRefCell<AudioInstance>>,
     pub _bgm: Option<Box<AudioInstance>>,
-    pub _device: Option<AudioDevice<Sound>>
+    pub _stream: rodio::OutputStream,
+    pub _stream_handle: rodio::OutputStreamHandle,
 }
 
 impl AudioInstance {
@@ -74,12 +38,22 @@ impl AudioInstance {
 
 impl ProjectAudioManager {
     pub fn create_audio_manager() -> Box<ProjectAudioManager> {
+        let device = std::thread::spawn(|| cpal::default_host()
+            .default_output_device()
+            .ok_or(StreamError::NoDevice).expect("")).join().unwrap();
+
+        // let device = std::thread::spawn(|| cpal::default_host().default_output_device().unrwap())
+        //     .join()
+        //     .unwrap();
+        let (stream, stream_handle) = rodio::OutputStream::try_from_device(&device).unwrap();
+
         Box::new(ProjectAudioManager {
             _project_application: std::ptr::null(),
             _project_resources: std::ptr::null(),
+            _stream: stream,
+            _stream_handle: stream_handle,
             _audios: Vec::new(),
             _bgm: None,
-            _device: None,
         })
     }
 
@@ -102,42 +76,9 @@ impl ProjectAudioManager {
         let audio_instance = AudioInstance::create_audio(&audio_data);
         self._audios.push(audio_instance.clone());
 
-        //
-        let wav_file = Path::new("resource/sounds/game_load.wav");
-        let sdl_context = sdl2::init().expect("");
-        let audio_subsystem = sdl_context.audio().expect("");
-        let loaded_contents = system::load(&wav_file);
-
-        let desired_spec = AudioSpecDesired {
-            freq: Some(44_100),
-            channels: Some(1), // mono
-            samples: None,     // default
-        };
-
-        self._device = Some(audio_subsystem.open_playback(None, &desired_spec, |spec| {
-            let wav = AudioSpecWAV::load_wav(wav_file).expect("Could not load test WAV file");
-
-            let cvt = AudioCVT::new(
-                wav.format,
-                wav.channels,
-                wav.freq,
-                spec.format,
-                spec.channels,
-                spec.freq,
-            ).expect("Could not convert WAV file");
-
-            let data = cvt.convert(wav.buffer().to_vec());
-
-            // initialize the audio callback
-            Sound {
-                data,
-                volume: 0.25,
-                pos: 0,
-            }
-        }).expect(""));
-
-        // Start playback
-        self._device.as_ref().unwrap().resume();
+        let loaded_contents = system::load("resource/sounds/game_load.wav");
+        let source = rodio::Decoder::new(BufReader::new(loaded_contents)).unwrap();
+        self._stream_handle.play_raw(source.convert_samples());
 
         audio_instance
     }
