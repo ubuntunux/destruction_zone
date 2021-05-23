eeb5f88a5bf748107ebb139f30c8d5ec3d24dcae
ubuntunux <ubuntunux@gmail.com>
2021-05-23 23:20:33 +0900

    Revert "update rodio multithread test"
    This reverts commit e3a8043e

--- src/application/project_audio_manager.rs
+++ src/application/project_audio_manager.rs
@@ -1,17 +1,44 @@
-use std::io::{ Cursor, BufReader };
-
-use rodio::{self, source::Source, StreamError};
-use cpal;
-use cpal::traits::HostTrait;
-
 use rust_engine_3d::utilities::system::{ self, newRcRefCell, RcRefCell };
 
 use crate::application::project_application::ProjectApplication;
 use crate::resource::project_resource::ProjectResources;
 
+
+//
+extern crate sdl2;
+
+use sdl2::audio::{AudioCVT, AudioCallback, AudioSpecDesired, AudioSpecWAV};
+use std::path::{Path, PathBuf};
+use self::sdl2::audio::AudioDevice;
+
+// NOTE: You probably want to investigate the
+// mixer feature for real use cases.
+pub struct Sound {
+    data: Vec<u8>,
+    volume: f32,
+    pos: usize,
+}
+
+impl AudioCallback for Sound {
+    type Channel = u8;
+
+    fn callback(&mut self, out: &mut [u8]) {
+        for dst in out.iter_mut() {
+            let pre_scale = *self.data.get(self.pos).unwrap_or(&128);
+            let scaled_signed_float = (pre_scale as f32 - 128.0) * self.volume;
+            let scaled = (scaled_signed_float + 128.0) as u8;
+            *dst = scaled;
+            self.pos += 1;
+        }
+    }
+}
+
+
+//
+
 pub struct AudioDataCreateInfo {
     pub _audio_name: String,
-    pub _audio_source: rodio::Decoder<BufReader<Cursor<Vec<u8>>>>,
+    pub _audio_source: bool,
 }
 
 #[derive(Clone)]
@@ -24,8 +51,7 @@ pub struct ProjectAudioManager {
     pub _project_resources: *const ProjectResources,
     pub _audios: Vec<RcRefCell<AudioInstance>>,
     pub _bgm: Option<Box<AudioInstance>>,
-    pub _stream: rodio::OutputStream,
-    pub _stream_handle: rodio::OutputStreamHandle,
+    pub _device: Option<AudioDevice<Sound>>
 }
 
 impl AudioInstance {
@@ -38,22 +64,12 @@ impl AudioInstance {
 
 impl ProjectAudioManager {
     pub fn create_audio_manager() -> Box<ProjectAudioManager> {
-        let device = std::thread::spawn(|| cpal::default_host()
-            .default_output_device()
-            .ok_or(StreamError::NoDevice).expect("")).join().unwrap();
-
-        // let device = std::thread::spawn(|| cpal::default_host().default_output_device().unrwap())
-        //     .join()
-        //     .unwrap();
-        let (stream, stream_handle) = rodio::OutputStream::try_from_device(&device).unwrap();
-
         Box::new(ProjectAudioManager {
             _project_application: std::ptr::null(),
             _project_resources: std::ptr::null(),
-            _stream: stream,
-            _stream_handle: stream_handle,
             _audios: Vec::new(),
             _bgm: None,
+            _device: None,
         })
     }
 
@@ -76,9 +92,42 @@ impl ProjectAudioManager {
         let audio_instance = AudioInstance::create_audio(&audio_data);
         self._audios.push(audio_instance.clone());
 
-        let loaded_contents = system::load("resource/sounds/game_load.wav");
-        let source = rodio::Decoder::new(BufReader::new(loaded_contents)).unwrap();
-        self._stream_handle.play_raw(source.convert_samples());
+        //
+        let wav_file = Path::new("resource/sounds/game_load.wav");
+        let sdl_context = sdl2::init().expect("");
+        let audio_subsystem = sdl_context.audio().expect("");
+        let loaded_contents = system::load(&wav_file);
+
+        let desired_spec = AudioSpecDesired {
+            freq: Some(44_100),
+            channels: Some(1), // mono
+            samples: None,     // default
+        };
+
+        self._device = Some(audio_subsystem.open_playback(None, &desired_spec, |spec| {
+            let wav = AudioSpecWAV::load_wav(wav_file).expect("Could not load test WAV file");
+
+            let cvt = AudioCVT::new(
+                wav.format,
+                wav.channels,
+                wav.freq,
+                spec.format,
+                spec.channels,
+                spec.freq,
+            ).expect("Could not convert WAV file");
+
+            let data = cvt.convert(wav.buffer().to_vec());
+
+            // initialize the audio callback
+            Sound {
+                data,
+                volume: 0.25,
+                pos: 0,
+            }
+        }).expect(""));
+
+        // Start playback
+        self._device.as_ref().unwrap().resume();
 
         audio_instance
     }
