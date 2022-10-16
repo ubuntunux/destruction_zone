f6fcf0f81aacc37a8a60aafeb849cbb0ff78baa2
ubuntunux <ubuntunux@gmail.com>
2022-10-17 03:21:18 +0900

    update push constant

--- src/render_pass/render_gbuffer.rs
+++ src/render_pass/render_gbuffer.rs
@@ -1,9 +1,9 @@
 use std::path::PathBuf;
 use ash::vk;
+use rust_engine_3d::renderer::push_constants::PushConstant_RenderObject;
 use rust_engine_3d::renderer::renderer_data::RenderObjectType;
 use rust_engine_3d::resource::resource::RenderPassDataCreateInfoMap;
 use rust_engine_3d::vulkan_context::render_pass::PipelinePushConstantData;
-use crate::render_pass::push_constants::PushConstant_RenderShip;
 
 pub fn get_render_pass_data_create_info(render_object_type: RenderObjectType, render_pass_data_create_info_map: &mut RenderPassDataCreateInfoMap) {
     let render_pass_name = match render_object_type {
@@ -19,7 +19,7 @@ pub fn get_render_pass_data_create_info(render_object_type: RenderObjectType, re
         PipelinePushConstantData {
             _stage_flags: vk::ShaderStageFlags::ALL,
             _offset: 0,
-            _push_constant: Box::new(PushConstant_RenderShip::default())
+            _push_constant: Box::new(PushConstant_RenderObject::default())
         }
     ];
     render_pass_data_create_info._pipeline_data_create_infos.push(pipeline_data_create_info);
